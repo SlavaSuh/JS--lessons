e9ebb38ebe75f579f5091c2d26606e0000e355f0
SlavaSuh <veaceslavsuholitco@gmail.com>
2018-07-28 11:49:13 +0100

    Fourth Task Moderated

--- Lesson -2/main.js
+++ Lesson -2/main.js
@@ -44,41 +44,44 @@ alert(taskThird());
 
 // 4) Присвоить переменной а значение в промежутке [0..15]. С помощью оператора switch организовать вывод чисел от a до 15.
 var a = parseInt(prompt("Введите а :"));
-switch (a) {
-    case 0:
-        console.log("0");
-    case 1:
-        console.log("1");
-    case 2:
-        console.log("2");
-    case 3:
-        console.log("3");
-    case 4:
-        console.log("4");
-    case 5:
-        console.log("5");
-    case 6:
-        console.log("6");
-    case 7:
-        console.log("7");
-    case 8:
-        console.log("8");
-    case 9:
-        console.log("9");
-    case 10:
-        console.log("10");
-    case 11:
-        console.log("11");
-    case 12:
-        console.log("12");
-    case 13:
-        console.log("13");
-    case 14:
-        console.log("14");
-    case 15:
-        console.log("15");
+if (a >=0 && a <=15) { 
+    switch (a) {
+        case 0:
+            console.log("0");
+        case 1:
+            console.log("1");
+        case 2:
+            console.log("2");
+        case 3:
+            console.log("3");
+        case 4:
+            console.log("4");
+        case 5:
+            console.log("5");
+        case 6:
+            console.log("6");
+        case 7:
+            console.log("7");
+        case 8:
+            console.log("8");
+        case 9:
+            console.log("9");
+        case 10:
+            console.log("10");
+        case 11:
+            console.log("11");
+        case 12:
+            console.log("12");
+        case 13:
+            console.log("13");
+        case 14:
+            console.log("14");
+        case 15:
+            console.log("15");
+    }
+} else {
+    alert("Данное число не входит в диапазон от 0 до 15");
 }
-
 // 5) Реализовать основные 4 арифметические операции в виде функций с двумя параметрами. 
 // Обязательно использовать оператор return.
 
